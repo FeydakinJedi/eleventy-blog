2d3ec61f1a29a6ad3d8cf43ab310d7c6f0679046
Deonna Hodges <dnhodges@gmail.com>
2025-01-31 20:51:28 -0500

    fixing the placeholders

--- .eleventy.js
+++ .eleventy.js
@@ -25,10 +25,10 @@ module.exports = function(eleventyConfig) {
   // Hero Placeholder Shortcode
   eleventyConfig.addShortcode("heroPlaceholder", function(title) {
     console.log("Generating placeholder for:", title);
-    return `<svg width="1200" height="675" viewBox="0 0 1200 675" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="1200" height="675" fill="#2D3047" />
-      <rect x="100" y="187.5" width="1000" height="300" fill="#FF3B6F" fill-opacity="0.1" rx="20" />
-      <text x="600" y="337.5" font-family="system-ui" font-size="36" fill="#FF3B6F" text-anchor="middle"
+    return `<svg width="100%" height="100%" viewBox="0 0 16 9" preserveAspectRatio="xMidYMid slice" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="16" height="9" fill="#2D3047" />
+      <rect x="1.5" y="2.5" width="13" height="4" fill="#FF3B6F" fill-opacity="0.1" rx="0.5" />
+      <text x="8" y="5" font-family="system-ui" font-size="0.8" fill="#FF3B6F" text-anchor="middle"
         dominant-baseline="middle">
         ${title}
       </text>
